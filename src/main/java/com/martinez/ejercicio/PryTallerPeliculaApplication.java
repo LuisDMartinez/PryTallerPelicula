357d7197942d18b666370cf1052beb4f9ae0bc6c
unknown <ldmartinez3@espe.edu.ec>
2020-06-15 09:53:00 -0500

    creacion del proyecto

---
+++ src/main/java/com/martinez/ejercicio/PryTallerPeliculaApplication.java
@@ -0,0 +1,13 @@
+package com.martinez.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryTallerPeliculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryTallerPeliculaApplication.class, args);
+	}
+
+}
